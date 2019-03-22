1467d3cf35264daaa1c818ddaf6b88ada0995578
Nitin K <39520442+nkpydev@users.noreply.github.com>
2019-03-22 19:32:13 +0530

    Commit

---
+++ Selection Sort/Selection_Sort.md
@@ -0,0 +1,11 @@
+# Selection Sort Info
+
+**Selection Sort**: 
+> Selection Sort is the improvement over Bubble Sort by making only one exchange for every pass thr' the list.
+> It looks for the largest value in the list and places it at the last position as it makes a pass
+> This method requires __(n-1)__ passes to sort __n__ items
+
+### Example:
+- (26 54 **93** 17 77 31 44 55 **20**) --> (26 54 **20** 17 77 31 44 55 **93**)
+- (26 54 20 17 **77** 31 44 **55** 93) --> (26 54 20 17 **55** 31 44 **77** **93**)
+- so on..
---
+++ Selection Sort/selection_sort.py
@@ -0,0 +1,20 @@
+
+def sort_selection(user_input):
+    l = len(user_input)
+    for i in range(l-1,0,-1):
+        max = 0
+        for j in range(1,i+1):
+            if user_input[j]>user_input[max]:
+                max = j
+        user_input[max],user_input[i] = user_input[i],user_input[max]
+    
+    return user_input
+
+
+
+if __name__ == '__main__':
+    final_sorted = []
+    get_user_input = [int(x) for x in input('\nEnter numbers to be sorted [seperated by ","]:').split(',')] #int(x) is import as we are looking forward to integers and not string value
+    print('\nUser Input before Sorting:\t',get_user_input)
+    final_sorted = sort_selection(get_user_input)
+    print('\nUser Input after Bubble Sort:\t',get_user_input)
